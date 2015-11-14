6073e79d72435e7f873a8785c4223ec3ed8854cc
Heeyoon <heeyoon@mit.edu>
2015-11-14 16:50:26 -0500

    repushing base file for listing model

--- Cintern/models/listing.js
+++ Cintern/models/listing.js
@@ -1,16 +1,14 @@
 var mongoose = require("mongoose");
-/*
-This is the schema for the database that stores all the job listings on our site.  
-Each listing includes the employer id, the associated id number for the custom 
-application template present in the listing, the title of the listing, description 
-of the position, requirements to apply to the job, deadline, and a list of students 
-who have applied to the listing. 
-*/
+/**
+ * This is the schema for the database that stores all the job listings on our site.  
+ * Each listing includes the employer id, the associated id number for the custom 
+ * application template present in the listing, the title of the listing, description 
+ * of the position, requirements to apply to the job, and deadline.
+ */
 
 //Schema for listings
 var listingSchema = mongoose.Schema({
 	employerId: String, //user id of employer
-	customAppId: Number, //application id
 	title: String,
 	description: String,
 	requirements: [String],
@@ -18,19 +16,52 @@ var listingSchema = mongoose.Schema({
 });
 
 
+/** Heeyoon */
+
 /* Called when employer creates a new listing.  students would be empty initially, since one must create
 the listing before students can apply
 */
 listingSchema.statics.createListing = function(currEmployerId, applicationId, title, desc, reqs, deadline, callback) {
-	return Listing.create({
+	Listing.create({
 		employerId: currEmployerId,
 		customAppId: applicationId,
 		title: title,
 		description: desc,
 		requirements: reqs,
 		deadline: deadline
-	}, callback);
+	}, function(err, user) {
+	      if (err) {
+	        console.log(err);
+	        callback();
+	      } else {
+	        callback();
+	      }
+    });
 };
+
+listingSchema.statics.deleteListing = function(listingId) {
+
+};
+
+listingSchema.statics.getApplicantsForListing = function(listingId) {
+
+};
+
+
+/** Maddie */
+listingSchema.statics.getAllListings = function(listingId) {
+
+};
+
+listingSchema.statics.filterListings = function(query) {
+
+};
+
+listingSchema.statics.getAllEmployerListings = function(employerId) {
+
+};
+
+
 
 
 
