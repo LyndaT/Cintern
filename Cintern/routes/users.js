fdfab8cf804ac8099a651750938ab205ee8ffd98
Lynda Tang <lyndat@mit.edu>
2015-11-15 18:55:29 -0500

    Revert to copy

--- Cintern/routes/users.js
+++ Cintern/routes/users.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var user = require('../controllers/user');
-var student = require('../controllers/student');
-var employer = require('../controllers/employer');
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-router.post('/student', student.addStudent);
-
-router.post('/employer', employer.addEmployer);
 
 module.exports = router;
