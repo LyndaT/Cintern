349988b9285b74110f5a235e38f93943fd94bcea
Lynda Tang <lyndat@mit.edu>
2015-11-18 16:42:35 -0500

    Employer Student Controllers

--- Cintern/controllers/employer.js
+++ Cintern/controllers/employer.js
@@ -8,42 +8,31 @@
 var ObjectID = require('mongodb').ObjectID;
 var Employer = require('../models/Employer.js');
 var User = require('../models/User.js');
+var utils = require('../utils/utils');
 
 /**
  * POST /users/employers
  * Adds an Employer to the Database
- * @param {Object} Request body:
+ * 
+ * Request parameters:
  * 		-email: the inputed email of the Employer
  * 		-password: the inputed password of the Employer
  * 		-company: the inputed company of the Employer
- * @param {Object} Response:
- * 		-employer: the Employer that is added
+ * 
+ * Response:
+ * 		-success: true if the server succeeded in adding the Employer
+ * 		-err: on failure (i.e failed in adding Employer), an error message
  */
-module.exports.addEmployer = function(req, res, next){
+module.exports.createEmployer = function(req, res, next){
 	var email = req.body.email;
 	var password = req.body.password;
 	var company = req.body.company;
-	//TODO: Change
 	Employer.createEmployer(email, password, company, function(errMsg, employer){
 		if (errMsg){
-			
-		}
-	});
-	
-	User.addUser(email, password, false, function(errMsg, user){
-		if (errMsg) {
-			res.send({msg: errMsg});
+			utils.sendErrResponse(res, 403, errMsg);
 		} else {
-		    // req.session.email = email;
-			// res.send(result);
-			Employer.createEmployer(user._id, "Google", function(errMsg, employer){
-				if (errMsg){
-					res.send({msg: errMsg});
-				} else {
-					req.session.email = email;
-					res.send(employer);
-				}
-			});
+			req.session.id = employer.user;
+			utils.sendSuccessResponse(res);
 		}
 	});
 };
--- Cintern/controllers/student.js
+++ Cintern/controllers/student.js
@@ -9,12 +9,24 @@ var User = require('../models/User.js');
 /**
  * POST /users/students
  * Adds an Employer to the Database
- * @param {Object} Request body:
+ * 
+ * Request parameters:
  * 		-email: the inputed email of the Employer
  * 		-password: the inputed password of the Employer
- * @param {Object} Response:
- * 		-employer: the Student that is added
+ * 
+ * Response:
+ * 		-success: true if the server succeeded in adding to Student
+ * 		-err: on failure (i.e failed in adding Student), an error message
  */
 module.exports.createStudent = function(req, res, next){
-	
+	var email = req.body.email;
+	var password = req.body.password;
+	Student.createStudent(email, password, function(errMsg, student){
+		if (errMsg){
+			utils.sendErrorResponse(res, 403, errMsg);
+		} else {
+			req.session.id = student.user;
+			utils.sendSuccessResponse(res);
+		}
+	});
 };
