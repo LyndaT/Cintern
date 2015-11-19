2adc8e81aefbb197155ae4717732365affc3580f
Jennifer Wu <jennwu@mit.edu>
2015-11-18 20:28:55 -0500

    commiting to merge common populate stuff

--- Cintern/controllers/application.js
+++ Cintern/controllers/application.js
@@ -3,20 +3,13 @@ var utils = require('../utils/utils');
 
 exports.getCommon = function(req, res, next) {
 	var currentUser = req.session.user;
-	var isStudent = currentUser.isStudent;
-	if (isStudent) {
-		Common.
-	}
-};
+	if (currentUser) {
+		var isStudent = currentUser.isStudent;
+		var currentUserId = currentUser.userId;
+		var listingId = req.body.lstgid
+		var userId = req.params.userid
 
-exports.getCustom = function(req, res, next) {
-	var currentUser = req.session.user;
-	var isStudent = currentUser.isStudent;
-	var currentUserId = currentUser.userId;
-	var customId = req.params.lstgid
-	var userId = req.params.userid
-	if (isStudent) {
-		Custom.getIfOwner(currentUserId, customId, function(errMsg, custom) {
+		var doAfterGetCommon = function(errMsg, custom) {
 			if (errMsg) utils.sendErrResponse(res, 403, errMsg);
 			else if (!custom) utils.sendErrResponse(res, 403, "No custom");
 			else {
@@ -36,11 +29,46 @@ exports.getCustom = function(req, res, next) {
 					}
 				});
 			}
-		});
-	} else {
-		Listing.getAllEmployerListings(currentUserId, function(errMsg, listings) {
+		}
 
-		});
-		Custom.getStarOrSubmCustomForListing()
-	}
+		if (isStudent) Custom.getIfOwner(currentUserId, customId, doAfterGetCustom(errMsg, custom));
+		else Custom.getStarOrSubmCustomForListing(userId, listingId, doAfterGetCustom(errMsg, custom));
+	} 
+	else utils.sendErrResponse(res, 403, "No current user");
+};
+
+exports.getCustom = function(req, res, next) {
+	var currentUser = req.session.user;
+	if (currentUser) {
+		var isStudent = currentUser.isStudent;
+		var currentUserId = currentUser.userId;
+		var listingId = req.body.lstgid
+		var userId = req.params.userid
+
+		var doAfterGetCustom = function(errMsg, custom) {
+			if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+			else if (!custom) utils.sendErrResponse(res, 403, "No custom");
+			else {
+				custom.populateCustom(function(errMsg, custom) {
+					if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+					else if (!custom) utils.sendErrResponse(res, 403, "No custom");
+					else {
+						var content = {
+							"listing" : custom.listing,
+							"state" : custom.state,
+							"application" : custom.application,
+							"owner" : custom.owner,
+							"isTemplate" : custom.isTemplate,
+							"submitTime" : custom.submitTime
+						};
+						utils.sendSuccessResponse(res, content);
+					}
+				});
+			}
+		}
+
+		if (isStudent) Custom.getIfOwner(currentUserId, customId, doAfterGetCustom(errMsg, custom));
+		else Custom.getStarOrSubmCustomForListing(userId, listingId, doAfterGetCustom(errMsg, custom));
+	} 
+	else utils.sendErrResponse(res, 403, "No current user");
 };
