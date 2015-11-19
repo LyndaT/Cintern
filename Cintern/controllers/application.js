e8bed111d7392222422da22c3a5e6853af160060
Jennifer Wu <jennwu@mit.edu>
2015-11-18 21:04:28 -0500

    Adding back controllers

--- Cintern/controllers/application.js
+++ Cintern/controllers/application.js
@@ -1,9 +1,81 @@
+/**
+ * @author Jennifer Wu
+ *
+ * Application controller
+ */
 var Application = require('../models/application.js');
+var utils = require('../utils/utils');
 
+/**
+ * GET /users/applications/common/:userid
+ *
+ * Gets the common application associated with the userId
+ * 
+ * Request body:
+ * 	- userid: userId of the user's whose Common we need
+ */
 exports.getCommon = function(req, res, next) {
+	var currentUser = req.session.user;
+	if (currentUser) {
+		var userId = req.body.userid
 
+		Common.getCommonByOwnerId(userId, function(errMsg, common) {
+			if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+			else if (!custom) utils.sendErrResponse(res, 403, "No custom");
+			else {
+				common.populateCommon(function(errMsg, common) {
+					if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+					else if (!common) utils.sendErrResponse(res, 403, "No custom");
+					else {
+						var content = {
+							"application" : common.application,
+						};
+						utils.sendSuccessResponse(res, content);
+					}
+				});
+			}
+		});
+	} 
+	else utils.sendErrResponse(res, 403, "No current user");
 };
 
+/**
+ * GET /users/applications/custom/:userid/:lstgid
+ *
+ * Gets the custom application associated with the userId and lstgid
+ * 
+ * Request body:
+ * 	- userid: userId of the user's whose Custom we need
+ *	- lstgid: listingId of the listing's whose Custom we need
+ */
 exports.getCustom = function(req, res, next) {
+	var currentUser = req.session.user;
+	if (currentUser) {
+		var isStudent = currentUser.isStudent;
+		var listingId = req.body.lstgid;
+		var userId = req.body.userid;
 
+		Custom.getByOwnerAndListing(userId, listingId, isStudent, function(errMsg, custom) {
+			if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+			else if (!custom) utils.sendErrResponse(res, 403, "No custom");
+			else {
+				custom.populateCustom(function(errMsg, custom) {
+					if (errMsg) utils.sendErrResponse(res, 403, errMsg);
+					else if (!custom) utils.sendErrResponse(res, 403, "No custom");
+					else {
+						var content = {
+							"listing" : custom.listing,
+							"state" : custom.state,
+							"application" : custom.application,
+							"owner" : custom.owner,
+							"isTemplate" : custom.isTemplate,
+							"submitTime" : custom.submitTime
+						};
+						utils.sendSuccessResponse(res, content);
+					}
+				});
+			}
+		});
+	} 
+	else utils.sendErrResponse(res, 403, "No current user");
 };
