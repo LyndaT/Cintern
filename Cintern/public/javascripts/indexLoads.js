5649d56237cfb0bc7b6630028ba4ecb3950a8c8d
Jennifer Wu <jennwu@mit.edu>
2015-11-21 14:21:12 -0500

    remove indexloads extras

--- Cintern/public/javascripts/indexLoads.js
+++ Cintern/public/javascripts/indexLoads.js
@@ -5,9 +5,6 @@
  * @param{String} template
  * @param{Object} data
  */
-Handlebars.registerPartial('listing', Handlebars.templates['s_listing_row']);
-Handlebars.registerPartial('applicant', Handlebars.templates['e_applicants_row']);
-
 var loadPage = function(template, data) {
 	data = data || {};
 	$('#main-container').html(Handlebars.templates[template](data));
@@ -19,21 +16,6 @@ var loadPage = function(template, data) {
 var loadHomePage = function() {
 	loadPage('index');
 };
-
-var loadStudentViewListingsPage = function() {
-	$.get('/students/listings', function(response) {
-		console.log(response.content.listings[0]);
-		loadPage('s_listings', {listings: response.content.listings});
-	});
-};
-
-var loadEmployerViewApplicantsPage = function() {
-	$.get('/employers/applications/listings/564e920d0dc0a354f80f8692', function(response) {
-		console.log(response.content.applicants)
-		loadPage('e_applicants', {applicants: response.content.applicants});
-	});
-};
-
 
 // load the home page
 $(document).ready(function() {
