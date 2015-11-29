c5a3309c722f1748e6763560c6cfc53721373520
Lynda Tang <lyndat@mit.edu>
2015-11-29 01:31:06 -0500

    Brute force now sends error messages if called too many times.

--- Cintern/routes/users.js
+++ Cintern/routes/users.js
@@ -7,6 +7,7 @@ var user = require('../controllers/user');
 var router = express.Router();
 var student = require('../controllers/student');
 var employer = require('../controllers/employer');
+var utils = require('../utils/utils');
 
 var ExpressBrute = require('express-brute'),
     MongoStore = require('express-brute-mongo'),
@@ -14,51 +15,35 @@ var ExpressBrute = require('express-brute'),
     moment = require('moment'),
     store;
     
-var store = new MongoStore(function (ready) {
-  MongoClient.connect('mongodb://localhost/cintern', function(err, db) {
-    if (err) throw err;
-    ready(db.collection('bruteforce-store'));
-  });
-});
-    
-/**
- * Slows requests after 5 failed attempts
- */
-var userBruteforce = new ExpressBrute(store, {
-    freeRetries: 5,
-    proxyDepth: 1,
-    minWait: 1*60*1000, // 5 minutes 
-    maxWait: 60*60*1000, // 1 hour, 
-    failCallback: failCallback,
-    handleStoreError: handleStoreError
-});
-
-// No more than 1000 login attempts per day per IP 
-var globalBruteforce = new ExpressBrute(store, {
-    freeRetries: 1000,
-    proxyDepth: 1,
-    attachResetToRequest: false,
-    refreshTimeoutOnRequest: false,
-    minWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    maxWait: 25*60*60*1000, // 1 day 1 hour (should never reach this wait time) 
-    lifetime: 24*60*60, // 1 day (seconds not milliseconds) 
-    failCallback: failCallback,
-    handleStoreError: handleStoreError
-});
+// var store = new MongoStore(function (ready) {
+  // MongoClient.connect('mongodb://localhost/cintern', function(err, db) {
+    // if (err) throw err;
+    // ready(db.collection('bruteforce-store'));
+  // });
+// });
+var store = new ExpressBrute.MemoryStore();
 
 var failCallback = function (req, res, next, nextValidRequestDate) {
-    req.flash('error', "You've made too many failed attempts in a short period of time, please try again "+moment(nextValidRequestDate).fromNow());
-    res.redirect('/'); // brute force protection triggered, send them back to the login page 
+	res.status(429).json({
+		success: false,
+		err: "You have been locked out due to too many failed login attempts. Please try again later."
+	}).end();
 };
+    
+// /**
+ // * Slows requests after 3 failed attempts
+ // */
+// var userBruteforce = new ExpressBrute(store);
 
-var handleStoreError = function (error) {
-    log.error(error); // log this error so we can figure out what went wrong 
-    // cause node to exit, hopefully restarting the process fixes the problem 
-    throw {
-        message: error.message,
-        parent: error.parent
-    };
-};
+// Start slowing requests after 5 failed attempts to do something for the same user 
+var userBruteforce = new ExpressBrute(store, {
+    freeRetries: 4,
+    proxyDepth: 1,
+    minWait: 5*60*1000, // 5 minutes, 
+    maxWait: 60*60*1000, // 1 hour, 
+    failCallback: failCallback
+});
+
 
 // Add a given listing ID to the request body
 router.param('lstgid', function(req, res, next, listingId) {
@@ -73,12 +58,7 @@ router.param('userid', function(req, res, next, userId) {
 });
 
 /* POST login */
-router.post('/login', globalBruteforce.prevent, userBruteforce.getMiddleware({
-        key: function(req, res, next) {
-            // prevent too many attempts for the same username 
-            next(req.body.email);
-        }
-    }), user.login);
+router.post('/login', userBruteforce.prevent, user.login);
 
 /* POST logout */
 router.post('/logout', user.logout);
